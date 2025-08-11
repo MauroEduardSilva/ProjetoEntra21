e70060a0fb08c2db0f08612df3e8407ebe547fba
Mauro Eduard <mauroedu15@gmail.com>
2025-08-11 17:05:19 -0300

    Criei o JSP de sobre nos, cadastrar estabelecimento, ainda estou tentando configurar o Servlet, não está recebendo informações do formulario.

--- src/main/java/br/com/detalhamento/servelet/newServlet.java
+++ src/main/java/br/com/detalhamento/servelet/newServlet.java
@@ -14,8 +14,7 @@ public class newServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
   
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
-    		throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     	doGet(request, response);
     }
     
