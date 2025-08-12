5dada52ab9c082368a62481823a76756dd4b9d2f
Mauro Eduard Silva <mauroedu15@gmail.com>
2025-08-11 21:04:09 -0300

    Concluí a tarefa TRF-01.05, onde eu crio uma classe conquista do usuario, no modelo

---
+++ src/main/java/br/com/detalhamento/modelo/Conquista.java
@@ -0,0 +1,31 @@
+package br.com.detalhamento.modelo;
+
+public class Conquista {
+
+	//nome conquista;  descricao conquista; icone conquista
+	
+	private String nomeConquista;
+	private String descricaoConquista;
+	private String iconeConquista;
+	
+	public String getNomeConquista() {
+		return nomeConquista;
+	}
+	public void setNomeConquista(String nomeConquista) {
+		this.nomeConquista = nomeConquista;
+	}
+	
+	public String getDescricaoConquista() {
+		return descricaoConquista;
+	}
+	public void setDescricaoConquista(String descicaoConquista) {
+		this.descricaoConquista = descricaoConquista;
+	}
+	
+	public String getIconeConquista() {
+		return iconeConquista;
+	}
+	public void setIconeConquista(String iconeConquista) {
+		this.iconeConquista = iconeConquista;
+	}
+}
